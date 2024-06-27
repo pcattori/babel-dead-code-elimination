e9e91c152970eb04b350522df5af5a5a5a6d55bd
Pedro Cattori <pcattori@gmail.com>
2024-06-27 19:02:03 -0400

    refactor tests to use describe blocks

--- src/dead-code-elimination.test.ts
+++ src/dead-code-elimination.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "vitest"
+import { describe, expect, test } from "vitest"
 import { parse } from "@babel/parser"
 import generate from "@babel/generator"
 import dedent from "dedent"
@@ -13,131 +13,137 @@ let dce = (source: string): string => {
   return generate(ast).code
 }
 
-test("import:side-effect", () => {
-  let source = dedent`
-    import "side-effect"
-  `
-  expect(dce(source)).toMatchInlineSnapshot(`"import "side-effect";"`)
+describe("import", () => {
+  test("side-effect", () => {
+    let source = dedent`
+      import "side-effect"
+    `
+    expect(dce(source)).toMatchInlineSnapshot(`"import "side-effect";"`)
+  })
+
+  test("named", () => {
+    let source = dedent`
+      import { a, _b } from "named"
+      import { _c } from "named-unused"
+      console.log(a)
+    `
+    expect(dce(source)).toMatchInlineSnapshot(`
+      "import { a } from "named";
+      console.log(a);"
+    `)
+  })
+
+  test("default", () => {
+    let source = dedent`
+      import a from "default-used"
+      import _b from "default-unused"
+      console.log(a)
+    `
+    expect(dce(source)).toMatchInlineSnapshot(`
+      "import a from "default-used";
+      console.log(a);"
+    `)
+  })
+
+  test("namespace", () => {
+    let source = dedent`
+      import * as a from "namespace-used"
+      import * as _b from "namespace-unused"
+      console.log(a)
+    `
+    expect(dce(source)).toMatchInlineSnapshot(`
+      "import * as a from "namespace-used";
+      console.log(a);"
+    `)
+  })
 })
 
-test("import:named", () => {
-  let source = dedent`
-    import { a, _b } from "named"
-    import { _c } from "named-unused"
-    console.log(a)
-  `
-  expect(dce(source)).toMatchInlineSnapshot(`
-    "import { a } from "named";
-    console.log(a);"
-  `)
+describe("function", () => {
+  test("declaration", () => {
+    let source = dedent`
+      export function a() {
+        return
+      }
+      function _b() {
+        return
+      }
+    `
+    expect(dce(source)).toMatchInlineSnapshot(`
+      "export function a() {
+        return;
+      }"
+    `)
+  })
+
+  test("expression", () => {
+    let source = dedent`
+      export let a = function () {
+        return
+      }
+      let _b = function () {
+        return
+      }
+    `
+    expect(dce(source)).toMatchInlineSnapshot(`
+      "export let a = function () {
+        return;
+      };"
+    `)
+  })
+
+  test("arrow", () => {
+    let source = dedent`
+      export let a = () => {
+        return
+      }
+      let _b = () => {
+        return
+      }
+    `
+    expect(dce(source)).toMatchInlineSnapshot(`
+      "export let a = () => {
+        return;
+      };"
+    `)
+  })
 })
 
-test("import:default", () => {
-  let source = dedent`
-    import a from "default-used"
-    import _b from "default-unused"
-    console.log(a)
-  `
-  expect(dce(source)).toMatchInlineSnapshot(`
-    "import a from "default-used";
-    console.log(a);"
-  `)
-})
+describe("variable", () => {
+  test("identifier", () => {
+    let source = dedent`
+      let a = "a"
+      let _b = "b"
+      console.log(a)
+    `
+    expect(dce(source)).toMatchInlineSnapshot(`
+      "let a = "a";
+      console.log(a);"
+    `)
+  })
 
-test("import:namespace", () => {
-  let source = dedent`
-    import * as a from "namespace-used"
-    import * as _b from "namespace-unused"
-    console.log(a)
-  `
-  expect(dce(source)).toMatchInlineSnapshot(`
-    "import * as a from "namespace-used";
-    console.log(a);"
-  `)
-})
+  test("array pattern", () => {
+    let source = dedent`
+      let [a, _b] = c
+      console.log(a)
+    `
+    expect(dce(source)).toMatchInlineSnapshot(`
+      "let [a] = c;
+      console.log(a);"
+    `)
+  })
 
-test("function:declaration", () => {
-  let source = dedent`
-    export function a() {
-      return
-    }
-    function _b() {
-      return
-    }
-  `
-  expect(dce(source)).toMatchInlineSnapshot(`
-    "export function a() {
-      return;
-    }"
-  `)
-})
-
-test("function:expression", () => {
-  let source = dedent`
-    export let a = function () {
-      return
-    }
-    let _b = function () {
-      return
-    }
-  `
-  expect(dce(source)).toMatchInlineSnapshot(`
-    "export let a = function () {
-      return;
-    };"
-  `)
-})
-
-test("function:arrow", () => {
-  let source = dedent`
-    export let a = () => {
-      return
-    }
-    let _b = () => {
-      return
-    }
-  `
-  expect(dce(source)).toMatchInlineSnapshot(`
-    "export let a = () => {
-      return;
-    };"
-  `)
-})
-
-test("variable:identifier", () => {
-  let source = dedent`
-    let a = "a"
-    let _b = "b"
-    console.log(a)
-  `
-  expect(dce(source)).toMatchInlineSnapshot(`
-    "let a = "a";
-    console.log(a);"
-  `)
-})
-
-test("variable:array pattern", () => {
-  let source = dedent`
-    let [a, _b] = c
-    console.log(a)
-  `
-  expect(dce(source)).toMatchInlineSnapshot(`
-    "let [a] = c;
-    console.log(a);"
-  `)
-})
-
-test("variable:object pattern", () => {
-  let source = dedent`
-    let {a, _b} = c
-    console.log(a)
-  `
-  expect(dce(source)).toMatchInlineSnapshot(`
-    "let {
-      a
-    } = c;
-    console.log(a);"
-  `)
+  test("object pattern", () => {
+    let source = dedent`
+      let {a, _b} = c
+      console.log(a)
+    `
+    expect(dce(source)).toMatchInlineSnapshot(`
+      "let {
+        a
+      } = c;
+      console.log(a);"
+    `)
+  })
 })
 
 test("repeated elimination", () => {
