82dcffe2b572103a2f4865ff90a587c7376b04b0
Pedro Cattori <pcattori@gmail.com>
2024-06-27 15:35:27 -0400

    wip

--- src/remove-unused-variables.test.ts
+++ src/remove-unused-variables.test.ts
@@ -2,59 +2,245 @@ import { describe, expect, test } from "vitest"
 import { parse } from "@babel/parser"
 import generate from "@babel/generator"
 import dedent from "dedent"
+import * as prettier from "prettier"
 
 import removeUnusedVariables from "./remove-unused-variables"
 
-let transform = (source: string): string => {
+let transform = async (source: string): Promise<string> => {
   let ast = parse(source, { sourceType: "module" })
   removeUnusedVariables(ast)
-  return generate(ast).code
+  return prettier.format(generate(ast).code, { parser: "babel", semi: false })
 }
 
-describe("variable declarator", () => {
-  test("identifier", async () => {
+describe("VariableDeclarator", () => {
+  test("Identifier", async () => {
     let source = dedent`
       let _x = 1
-      export let x = 1
+      let x = 1
+      ref(x)
     `
-    expect(transform(source)).toMatchInlineSnapshot(`"export let x = 1;"`)
+    expect(await transform(source)).toMatchInlineSnapshot(`
+      "let x = 1
+      ref(x)
+      "
+    `)
   })
 
-  test("object pattern", async () => {
-    let source = dedent`
-      let _input = { _x: 1 }
-      let { _x, ..._restx } = _input
+  describe("ObjectPattern", () => {
+    test("within VariableDeclarator", async () => {
+      let source = dedent`
+        let { _a, a } = x
+        ref(a)
 
-      let input = { _a: 1, a: 1, _b: { _bb: 1 }, b: { bb: 1 }, c: { _cc: 1, cc: 1 } }
-      let { _a, a, _b: { _bb }, b: { bb }, c: { _cc, cc }, ...rest } = input
-      console.log(a, bb, cc, rest)
-    `
-    expect(transform(source)).toMatchInlineSnapshot(`
-      "let input = {
-        _a: 1,
-        a: 1,
-        _b: {
-          _bb: 1
-        },
-        b: {
-          bb: 1
-        },
-        c: {
-          _cc: 1,
-          cc: 1
-        }
-      };
-      let {
-        a,
-        b: {
-          bb
-        },
-        c: {
-          cc
-        },
-        ...rest
-      } = input;
-      console.log(a, bb, cc, rest);"
-    `)
+        let {..._rest} = x
+        let {...rest} = x
+        ref(rest)
+      `
+      expect(await transform(source)).toMatchInlineSnapshot(`
+        "let { a } = x
+        ref(a)
+        let { ...rest } = x
+        ref(rest)
+        "
+      `)
+    })
+
+    test("within ObjectProperty", async () => {
+      let source = dedent`
+        let { a: { _aa, aa, ..._rest } } = x
+        ref(aa)
+      `
+      expect(await transform(source)).toMatchInlineSnapshot(`
+        "let {
+          a: { aa },
+        } = x
+        ref(aa)
+        "
+      `)
+    })
+
+    test("within ArrayPattern", async () => {
+      let source = dedent`
+        let { a: [{ _aa, aa, ..._rest }] } = x
+        ref(aa)
+      `
+      expect(await transform(source)).toMatchInlineSnapshot(`
+        "let {
+          a: [{ aa }],
+        } = x
+        ref(aa)
+        "
+      `)
+    })
+
+    describe("within AssignmentPattern", () => {
+      test("within ObjectProperty", async () => {
+        let source = dedent`
+          let { a: { _aa, aa, ..._rest }={ aa: 1 } } = x
+          ref(aa)
+        `
+        expect(await transform(source)).toMatchInlineSnapshot(`
+          "let {
+            a: { aa } = {
+              aa: 1,
+            },
+          } = x
+          ref(aa)
+          "
+        `)
+      })
+
+      test("within ArrayPattern", async () => {
+        let source = dedent`
+          let { a: [{ _aa, aa, ...rest }={ aa: 1 }] } = x
+          ref(aa)
+        `
+        expect(await transform(source)).toMatchInlineSnapshot(`
+          "let {
+            a: [
+              { aa } = {
+                aa: 1,
+              },
+            ],
+          } = x
+          ref(aa)
+          "
+        `)
+      })
+    })
+
+    test("within RestElement", async () => {
+      let source = dedent`
+        let [...{ _a, a, ..._rest }] = x
+        ref(a)
+      `
+      expect(await transform(source)).toMatchInlineSnapshot(`
+        "let [...{ a }] = x
+        ref(a)
+        "
+      `)
+    })
+
+    test("unzips if all variables are unused", async () => {
+      let source = dedent`
+        let {
+          _a, // Identifier
+          _b: {_bb, ..._brest}, // within ObjectProperty
+          _c: [{_cc, ..._crest}], // within ArrayPattern
+          _d: {_dd, ..._drest} = {}, // within AssignmentPattern within ObjectProperty
+          _e: [{_ee, ..._erest}={}], // within AssignmentPattern within ArrayPattern
+          _f: [...{_ff, ..._frest}], // within RestElement
+          ..._g // RestElement
+        } = _x
+      `
+      expect(await transform(source)).toMatchInlineSnapshot(`""`)
+    })
+  })
+
+  describe("ArrayPattern", () => {
+    test("within VariableDeclarator", async () => {
+      let source = dedent`
+        let [ _a0, a1, _a2, a3, _a4 ] = x
+        ref(a1, a3)
+
+        let [..._rest] = x
+        let [...rest] = x
+        ref(rest)
+      `
+      expect(await transform(source)).toMatchInlineSnapshot(`
+        "let [, a1, , a3, ,] = x
+        ref(a1, a3)
+        let [...rest] = x
+        ref(rest)
+        "
+      `)
+    })
+
+    test("within ObjectProperty", async () => {
+      let source = dedent`
+        let { a: [ _aa, aa, ..._rest ] } = x
+        ref(aa)
+      `
+      expect(await transform(source)).toMatchInlineSnapshot(`
+        "let {
+          a: [, aa],
+        } = x
+        ref(aa)
+        "
+      `)
+    })
+
+    test("within ArrayPattern", async () => {
+      let source = dedent`
+        let [[ _aa, aa, ..._rest ]] = x
+        ref(aa)
+      `
+      expect(await transform(source)).toMatchInlineSnapshot(`
+        "let [[, aa]] = x
+        ref(aa)
+        "
+      `)
+    })
+
+    describe("within AssignmentPattern", () => {
+      test("within ObjectProperty", async () => {
+        let source = dedent`
+          let { a: { _aa, aa, ..._rest }={ aa: 1 } } = x
+          ref(aa)
+        `
+        expect(await transform(source)).toMatchInlineSnapshot(`
+          "let {
+            a: { aa } = {
+              aa: 1,
+            },
+          } = x
+          ref(aa)
+          "
+        `)
+      })
+
+      test("within ArrayPattern", async () => {
+        let source = dedent`
+          let [{ _a, a, ...rest }={ a: 1 }] = x
+          ref(a)
+        `
+        expect(await transform(source)).toMatchInlineSnapshot(`
+          "let [
+            { a } = {
+              a: 1,
+            },
+          ] = x
+          ref(a)
+          "
+        `)
+      })
+    })
+
+    test("within RestElement", async () => {
+      let source = dedent`
+        let [...[ _a, a, ..._rest ]] = x
+        ref(a)
+      `
+      expect(await transform(source)).toMatchInlineSnapshot(`
+        "let [...[, a]] = x
+        ref(a)
+        "
+      `)
+    })
+
+    test("unzips if all variables are unused", async () => {
+      let source = dedent`
+        let [
+          _a, // Identifier
+          {_b, ..._brest}, // within ObjectProperty
+          [{_c, ..._crest}], // within ArrayPattern
+          {_d, ..._drest} = {}, // within AssignmentPattern within ObjectProperty
+          [{_e, ..._erest}={}], // within AssignmentPattern within ArrayPattern
+          [...{_f, ..._frest}], // within RestElement
+          ..._g // RestElement
+        ] = _x
+      `
+      expect(await transform(source)).toMatchInlineSnapshot(`""`)
+    })
   })
 })
--- src/remove-unused-variables.ts
+++ src/remove-unused-variables.ts
@@ -1,89 +1,166 @@
 import {
   t,
   traverse,
+  type Node,
   type NodePath,
   type BabelTypes,
   type ParseResult,
 } from "./babel-esm"
 import * as Identifier from "./identifier"
 
+// TODO:
+// - [ ] function: decl, expr, arrow
+// - [ ] import
+// - [ ] catch
+// - [ ] class: decl, prop, method
+// - [ ] ts: enum decl, interface decl, type alias decl
+
 export default function (ast: ParseResult<BabelTypes.File>) {
   let changed: boolean
+
   do {
     changed = false
     traverse(ast, {
       Program(path) {
         path.scope.crawl()
       },
-      // function: decl, expr, arrow
-      // catch
-      // import
-      // class: decl, prop, method
-      // ts: enum decl, interface decl, type alias decl
       VariableDeclarator(path) {
-        if (t.isIdentifier(path.node.id)) {
-          let local = path.get("id") as NodePath<BabelTypes.Identifier>
-          if (!Identifier.isReferenced(local)) {
+        let id = path.get("id")
+
+        if (id.isIdentifier()) {
+          if (!Identifier.isReferenced(id)) {
             path.remove()
             changed = true
           }
           return
         }
 
-        if (t.isObjectPattern(path.node.id)) {
-          // rest: ...x
-          // assignment: {x=1}
-          let local = path.get("id") as NodePath<BabelTypes.ObjectPattern>
+        if (id.isObjectPattern() || id.isArrayPattern()) {
+          let vars = findVariablesInPattern(id)
+          for (let v of vars) {
+            if (Identifier.isReferenced(v)) continue
 
-          let identifiers = findVariablesInObjectPattern(local)
-          for (let identifier of identifiers) {
-            if (!Identifier.isReferenced(identifier)) {
-              identifier.parentPath.remove()
-              changed = true
+            let parent = v.parentPath
+            changed = true
 
-              let objectPattern = identifier.find((p) =>
-                p.isObjectPattern(),
-              ) as NodePath<t.ObjectPattern> | null
-              if (objectPattern && objectPattern.node.properties.length === 0) {
-                objectPattern.parentPath.remove()
-                // TODO: what about array pattern parent? and what if that parent has an obj pattern or array pattern parent, etc.?
-              }
+            if (parent.isObjectProperty()) {
+              parent.remove()
+              continue
             }
+
+            if (parent.isArrayPattern()) {
+              parent.node.elements[v.key as number] = null
+              continue
+            }
+
+            if (parent.isAssignmentPattern()) {
+              if (t.isObjectProperty(parent.parent)) {
+                parent.parentPath?.remove()
+                continue
+              }
+              if (t.isArrayPattern(parent.parent)) {
+                parent.parent.elements[parent.key as number] = null
+                continue
+              }
+              throw unsupported(parent)
+            }
+
+            if (parent.isRestElement()) {
+              parent.remove()
+              continue
+            }
+
+            throw unsupported(parent)
           }
+
           return
         }
-
-        if (t.isArrayPattern(path.node.id)) {
-          // rest: ...x
-          // assignment: [x=1]
+      },
+      ObjectPattern(path) {
+        let isWithinDeclarator =
+          path.find((p) => p.isVariableDeclarator()) !== null
+        let isEmpty = path.node.properties.length === 0
+        if (isWithinDeclarator && isEmpty) {
+          sweepPattern(path)
+          changed = true
+        }
+      },
+      ArrayPattern(path) {
+        let isWithinDeclarator =
+          path.find((p) => p.isVariableDeclarator()) !== null
+        let isEmpty = path.node.elements.every((e) => e === null)
+        if (isWithinDeclarator && isEmpty) {
+          sweepPattern(path)
+          changed = true
         }
       },
     })
   } while (changed)
 }
 
-function findVariablesInObjectPattern(
-  path: NodePath<t.ObjectPattern>,
+function findVariablesInPattern(
+  patternPath: NodePath<t.ObjectPattern | t.ArrayPattern>,
 ): NodePath<t.Identifier>[] {
   let variables: NodePath<t.Identifier>[] = []
-  function recurse(path: NodePath) {
+  function recurse(path: NodePath<Node | null>): void {
     if (path.isIdentifier()) {
-      return variables.push(path)
-    }
-    if (path.isObjectProperty()) {
-      return recurse(path.get("value"))
+      variables.push(path)
+      return
     }
     if (path.isObjectPattern()) {
       return path.get("properties").forEach(recurse)
     }
+    if (path.isObjectProperty()) {
+      return recurse(path.get("value"))
+    }
+    if (path.isArrayPattern()) {
+      let _elements = path.get("elements")
+      return _elements.forEach(recurse)
+    }
+    if (path.isAssignmentPattern()) {
+      return recurse(path.get("left"))
+    }
     if (path.isRestElement()) {
       return recurse(path.get("argument"))
     }
-    // TODO: assignment pattern!
-    path.buildCodeFrameError(
-      `Failed to find variables within object pattern due to unrecognized node type: ${path.node.type}`,
-    )
+    if (path.node === null) return
+    throw unsupported(path)
   }
-  recurse(path)
+  recurse(patternPath)
   return variables
+}
+
+function sweepPattern(path: NodePath<t.ObjectPattern | t.ArrayPattern>) {
+  let parent = path.parentPath
+  if (parent.isVariableDeclarator()) {
+    return parent.remove()
+  }
+  if (parent.isArrayPattern()) {
+    parent.node.elements[path.key as number] = null
+    return
+  }
+  if (parent.isObjectProperty()) {
+    return parent.remove()
+  }
+  if (parent.isRestElement()) {
+    return parent.remove()
+  }
+  if (parent.isAssignmentPattern()) {
+    if (t.isObjectProperty(parent.parent)) {
+      return parent.parentPath.remove()
+    }
+    if (t.isArrayPattern(parent.parent)) {
+      parent.parent.elements[parent.key as number] = null
+      return
+    }
+    throw unsupported(parent.parentPath)
+  }
+  throw unsupported(parent)
+}
+
+function unsupported(path: NodePath<Node | null>) {
+  let type = path.node === null ? "null" : path.node.type
+  return path.buildCodeFrameError(
+    `[babel-dead-code-elimination] Unsupported node type: ${type}`,
+  )
 }
